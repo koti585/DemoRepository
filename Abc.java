0be30e60f81814706abd08a2fa488921eb6baf30
koti585 <kotireddyputluri1@gmail.com>
2022-06-06 15:51:50 +0530

    Some Changes

--- Abc.java
+++ Abc.java
@@ -1,9 +1,9 @@
 Public class Abc
   {
      public static void main(String args[])
-       {   for(int i=0; i<=10; i++)
+       {   if(true)
              {
-               System.out.println("Just For Loop Demo");
+               System.out.println("Simply some changes are done");
              }
        }
 
